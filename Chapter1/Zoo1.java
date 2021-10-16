492116047a80f731bea5a346654fbe18758a0448
Teo Udrea <teodora.udrea1998@gmail.com>
2021-10-16 20:51:32 +0100

    Chapter1 first commit -p14

---
+++ Chapter1/Zoo1.java
@@ -0,0 +1,8 @@
+package Chapter1;
+
+public class Zoo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
---
+++ Chapter1/Zoo2.java
@@ -0,0 +1,10 @@
+package Chapter1;
+
+public class Zoo2 {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
+
+//run the program with the arguments of the array listed after the name of the program
