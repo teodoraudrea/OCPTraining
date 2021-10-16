13b1bd56117f1214a1b23c3d01fc3f4df063ddc5
Teo Udrea <teodora.udrea1998@gmail.com>
2021-10-16 20:52:13 +0100

    Chapter1 2 p14

---
+++ Chapter1/Animal.java
@@ -0,0 +1,11 @@
+package Chapter1;
+
+public class Animal {
+    String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName){
+        name = newName;
+    }
+}
